5ae496f0a8e950b4ec15510ad44d862acd37c80e
Usuario <Usuario@CPD02-M16>
2024-05-28 10:45:34 -0300

    fgfg

--- Funcoes/Program.cs
+++ Funcoes/Program.cs
@@ -8,88 +8,168 @@ namespace Funcoes
 {
     internal class Program
     {
-        static void arealosango()
+        // static void arealosango()
+       // {
+       //     Console.WriteLine("Informe a diagonal maior do losango: ");
+       //     double d = double.Parse(Console.ReadLine());
+
+       //     Console.WriteLine("Agora informe a diagonal menor do losango: ");
+       //     double D = double.Parse(Console.ReadLine());
+
+        //    double arealosango = (D * d) / 2;
+       //     Console.WriteLine("A área do losango é de: " + arealosango);
+      //  }
+
+     //   static void areaquadrado()
+      //  {
+     //       Console.WriteLine("Informe o valor da base do quadrado: ");
+      //      double h = double.Parse(Console.ReadLine());
+//
+      //      double areaquadrado = h * h;
+     //       Console.WriteLine("A área do quadrado é de: " + areaquadrado);
+     //
+     //   }
+    //    static void areatrapezio()
+    //    {
+     //       Console.WriteLine("Informe o valor da base maior: ");
+      //      double B = double.Parse(Console.ReadLine());
+
+     //       Console.WriteLine("Informe o valor da base menor");
+     //       double b = double.Parse(Console.ReadLine());
+
+       //     Console.WriteLine("Agora informe o valor da altura: ");
+        //    double alt = double.Parse(Console.ReadLine());
+
+         //   double areatrapezio = (b + B) * alt / 2;
+          //  Console.WriteLine("A área do trapézio é de: " + areatrapezio);
+       // }
+       // static void arearetangulo()
+        //{
+        //    Console.WriteLine("Informe a base do retangulo: ");
+        //    double b2 = double.Parse(Console.ReadLine());
+
+       //     Console.WriteLine("Agora informe a altura do retangulo: ");
+        //    double h2 = double.Parse(Console.ReadLine());
+
+        //    double arearetangulo = b2 * h2;
+        //    Console.WriteLine("A área do retangulo é: " + arearetangulo);
+       // }
+
+      //  static void Main(string[] args)
+      //  {
+      //      Console.WriteLine("Escolha alguma forma geométrica entre losango(1), quadrado(2), trapézio(3) e retangulo(4): ");
+         //   int forma = int.Parse(Console.ReadLine());
+
+
+           // switch (forma)
+        //    {
+         //       case 1:
+         //           arealosango();
+          //          break;
+          //
+          //      case 2:
+         //           areaquadrado();
+            //        break;
+
+         //       case 3:
+            //        areatrapezio();
+             //       break;
+
+            //    case 4:
+            //       arearetangulo();
+            //        break;
+
+             //   default:
+              //      Console.WriteLine("Opção inválida: ");
+                //    break;
+           // }
+
+           // Console.ReadKey();//
+
+        static void soma()
         {
-            Console.WriteLine("Informe a diagonal maior do losango: ");
-            double d = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o primeiro número: ");
+            double n1 = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Agora informe a diagonal menor do losango: ");
-            double D = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número: ");
+            double n2 = double.Parse(Console.ReadLine());
 
-            double arealosango = (D * d) / 2;
-            Console.WriteLine("A área do losango é de: " + arealosango);
+            double resultado = n1 + n2;
+
+            Console.WriteLine("O resultado será: " + resultado);
+
         }
 
-        static void areaquadrado()
+        static void multiplicacao()
         {
-            Console.WriteLine("Informe o valor da base do quadrado: ");
-            double h = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o primeiro número: ");
+            double n1 = double.Parse(Console.ReadLine());
 
-            double areaquadrado = h * h;
-            Console.WriteLine("A área do quadrado é de: " + areaquadrado);
+            Console.WriteLine("Digite o segundo número: ");
+            double n2 = double.Parse(Console.ReadLine());
+
+            double resultado = n1 * n2;
+
+            Console.WriteLine("O resultado será: " + resultado);
 
         }
-        static void areatrapezio()
+
+        static void divisao()
         {
-            Console.WriteLine("Informe o valor da base maior: ");
-            double B = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o primeiro número: ");
+            double n1 = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Informe o valor da base menor");
-            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número: ");
+            double n2 = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Agora informe o valor da altura: ");
-            double alt = double.Parse(Console.ReadLine());
+            double resultado = n1 / n2;
 
-            double areatrapezio = (b + B) * alt / 2;
-            Console.WriteLine("A área do trapézio é de: " + areatrapezio);
+            Console.WriteLine("O resultado será: " + resultado);
+
         }
-        static void arearetangulo()
+
+        static void subtracao()
         {
-            Console.WriteLine("Informe a base do retangulo: ");
-            double b2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o primeiro número: ");
+            double n1 = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Agora informe a altura do retangulo: ");
-            double h2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número: ");
+            double n2 = double.Parse(Console.ReadLine());
 
-            double arearetangulo = b2 * h2;
-            Console.WriteLine("A área do retangulo é: " + arearetangulo);
+            double resultado = n1 - n2;
+
+            Console.WriteLine("O resultado será: " + resultado);
+
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Escolha alguma forma geométrica entre losango(1), quadrado(2), trapézio(3) e retangulo(4): ");
-            int forma = int.Parse(Console.ReadLine());
 
 
-            switch (forma)
+            Console.WriteLine("Escolha uma operação digitando 1 para soma, 2 para multiplicação, 3 para divisão e 4 para subtração");
+            int operacao = int.Parse(Console.ReadLine());
+
+            switch (operacao)
             {
                 case 1:
-                    arealosango();
+                    soma();
                     break;
-
                 case 2:
-                    areaquadrado();
+                    multiplicacao();
                     break;
-
                 case 3:
-                    areatrapezio();
+                    divisao(); 
                     break;
-
-                case 4:
-                   arearetangulo();
+                case 4: 
+                    subtracao();
                     break;
-
                 default:
-                    Console.WriteLine("Opção inválida: ");
+                    Console.WriteLine("Opção inválida, tente novamente");
                     break;
             }
-
             Console.ReadKey();
-
-
-
-
-
         }
+
     }
-}
+    }
+
